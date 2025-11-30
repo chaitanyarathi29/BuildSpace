24cdc97e381fef94994cc97183f9e05d006d1653
Chaitanya Rathi <chaitanyarathi29@gmail.com>
2025-11-30 20:14:04 +0530

    reverseProxy and build-server completed

--- build-server/script.js
+++ build-server/script.js
@@ -2,18 +2,23 @@ const { exec } = require('child_process');
 const path = require('path');
 const fs = require('fs');
 const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
+const mime = require('mime-types');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const s3Client = new S3Client({
-    region: '',
+    region: 'eu-north-1',
     credentials: {
-        accessKeyId: '',
-        secretAccessKey: ''
+        accessKeyId: process.env.ACCESS_KEY_ID,
+        secretAccessKey: process.env.SECRET_ACCESS_KEY_ID
     }
 })
 
+const PROJECT_ID = process.env.PROJECT_ID;
+
 async function init() {
-    console.log("Execturing script.js");
-    const outDirPath = path(__dirname, 'output');
+    console.log("Executing script.js");
+    const outDirPath = path.join(__dirname, 'output');
 
     const p = exec(`cd ${outDirPath} && npm install && npm run build`)
 
@@ -25,16 +30,32 @@ async function init() {
         console.log("Error", data.toString());
     })
 
-    p.on('close', function() {
+    p.on('close', async function() {
         console.log('Build Complete');
         const distFolderPath = path.join(__dirname, 'output', 'dist');
         const distFolderContents = fs.readdirSync(distFolderPath, {recursive: true} )
-
-        for(const filePath of distFolderContents) {
+ 
+        for(const file of distFolderContents) {
+            const filePath = path.join(distFolderPath, file);
             if(fs.lstatSync(filePath).isDirectory()) continue;
+             
+            console.log("uploading", filePath);
 
-            
+            const command = new PutObjectCommand({
+                Bucket: 'buildspace-vercel-clone',
+                Key: `__outputs/${PROJECT_ID}/${file}`,
+                Body: fs.createReadStream(filePath),
+                ContentType: mime.lookup(filePath) 
+            })
+
+            await s3Client.send(command);
+
+            console.log("uploaded", filePath);
+
         }
+        console.log('Done!');
     }) 
 
 }
+
+init();
